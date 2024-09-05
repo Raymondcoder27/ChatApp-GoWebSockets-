eda2c5a63310b49156d5fae9e183d3117339f88d
MwebeCoder27 <mwebraymond13@gmail.com>
2024-09-05 12:08:58 +0300

    init

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
+import "fmt"
+
 type Server struct {
-	conns map[*websocket.conn]bool
+	conns map[*websocket.Conn]bool
+}
+
+func NewServer() *Server {
+	return &Server{
+		conns: make(map[*websocket.Conn]bool),
+	}
+}
+func (s *Server) handleWS(ws *websocket.Conn) {
+	fmt.Println("New incoming connection from client.", ws.RemoteAddr())
+
+	s.conns[ws] = true
+
+	s.readLoop(ws)
+}
+
+func readLoop(ws *websocket.Conn) {
+
 }
 
 func main() {
